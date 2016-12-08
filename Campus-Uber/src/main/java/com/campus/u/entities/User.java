535cac8e496a0b4c87d405c8ee837379f4965963
Eddy Jimenez <eduardojimenez9@yahoo.com>
2016-12-08 15:35:14 -0800

    added getters/setters in user class

--- Campus-Uber/src/main/java/com/campus/u/entities/User.java
+++ Campus-Uber/src/main/java/com/campus/u/entities/User.java
@@ -34,6 +34,61 @@ public class User {
     @JsonIgnore
     private String password;
 
+    public User(){
 
+    }
 
+    public User(String name, String username, String password) {
+        this.name = name;
+        this.username = username;
+        this.password = password;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
